48c16eca117dde6203fda0fffeeea9b3d0f477cd
maxmarvell <max.marvell@hotmail.co.uk>
2024-03-06 19:58:00 +0000

    new model to store relevent params on training

--- non_linear/utils/linked_list.py
+++ non_linear/utils/linked_list.py
@@ -1,10 +1,24 @@
-class Node: 
+from non_linear.fisher import FisherInformation
+from non_linear.qfisher import QuantumFisherInformation
+from numpy import ndarray
+
+class LearnModelData:
+    def __init__(self, 
+                 fisher_information:FisherInformation = None, 
+                 quantum_fisher_information:QuantumFisherInformation = None,
+                 y_pred:ndarray = None):
+        
+        self.fisher_information = fisher_information
+        self.quantum_fisher_information = quantum_fisher_information
+        self.y_pred = y_pred
+
+
+
+class Node(LearnModelData):
     def __init__(self, layer):
         self.layer = layer
         self.outputs = None
         self.next = None
-        self.fisher_information = None
-        self.quantum_fisher_information = None
 
     def __repr__(self):
         return self.data
