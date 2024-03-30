719c73bce1f913ffad0f17225e20157e4b573b00
maxmarvell <max.marvell@hotmail.co.uk>
2024-03-30 15:55:06 +0000

    adding function descriptions

--- non_linear/classifier.py
+++ non_linear/classifier.py
@@ -1,11 +1,9 @@
 from functools import partial
 import pennylane.numpy as np
-import pennylane as qml
 import jax.numpy as jnp
 import jax
 import optax
 import random
-import torch
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -16,17 +14,15 @@ from matplotlib.ticker import AutoMinorLocator
 from non_linear.utils import graph_utils
 
 from non_linear.models import qnn_compiler
-from non_linear.fourier import SampleFourierCoefficients
 from non_linear.fisher import FisherInformation
 from non_linear.qfisher import QuantumFisherInformation
 
-from non_linear.utils.linked_list import NNLinkedList, LearnModelData
-from non_linear.autoencoder import Autoencoder
+from non_linear.utils.linked_list import LearnModelData
 
 from numpy import ndarray
 import numpy as onp
 
-# Class for classification tasks
+# class for classification tasks (non convolutional architecture)
 class ClassifierQNN():
 
     def __init__(self, model: any, data: np.ndarray, target: np.ndarray, n_layers: int) -> None:
@@ -80,6 +76,19 @@ class ClassifierQNN():
     @partial(jax.jit, static_argnums=(0,))
     def calculate_ce_cost(self, X, y, theta):
 
+        '''
+            computes the cost of the current model parametrisation on the input data (can be training or testing),
+            note that the prediction has to be transposed due to the behaviour of QNODE in pennylane 0.34.1
+
+            args:
+                x: the training input data
+                y: the training target data
+                theta: the current parametrisation of the model
+
+            return:
+                cost: the computed cost, should evaluate to a scaler number unless batched
+        '''
+
         # get prediction
         yp = jnp.array(self.qnn(X, theta)).T
 
@@ -93,17 +102,47 @@ class ClassifierQNN():
 
     @partial(jax.jit, static_argnums=(0,))
     def optimizer_update(self, opt_state, params, x, y):
+
+        '''
+            computes the loss function and gradients in the parameter space according to cross entropy loss,
+            updates the optimiser,
+
+            args:
+                opt_state: the current optimizer state
+                params: the current model parameters
+                x: the training input data
+                y: the training target data
+
+            return:
+                params: the updated model parameters
+                opt_state: the update optimizer state
+                loss: the current loss of the model on the training set
+        '''
+
         loss, grads = jax.value_and_grad(lambda theta: self.calculate_ce_cost(x, y, theta))(params)
         updates, opt_state = self.optimizer.update(grads, opt_state)
         params = optax.apply_updates(params, updates)
         return params, opt_state, loss
 
-
     def learn_model(self, 
                     epochs: int, 
                     seed: int = random.randrange(1000), 
-                    lr:float = 0.04,
-                    save_data:bool = False):
+                    lr:float = 0.04):
+        
+        '''
+            learn the classification task with the preset model, this is repeated for a given number of epochs
+            at every epoch the ADAM optimizer is updated and so the parameters,
+            at every 10 epochs the loss calculation is reported,
+            at every 25 epochs a training data structure is initialised which stores parametrisations and fisher matricies
+            finally the model is trained and the fit params are saved to the class
+
+            params:
+                epochs: the number of times the model is re-trained to update parameters
+            
+            **kwargs:
+                seed: the random number seed generator (defaults random but can be selected for reproducability)
+                lr: the learning rate of the optimizer
+        '''
 
         self.optimizer = optax.adam(learning_rate=lr)
 
@@ -123,38 +162,25 @@ class ClassifierQNN():
         ##### FIT #####
         for epoch in range(epochs):
             params, opt_state, cost = self.optimizer_update(opt_state, params, self.X_train, self.y_train)
+
             if epoch % 5 == 0:
                 print(f'epoch: {epoch}\t cost: {cost}')
 
-            if (epoch % 25 == 0 and save_data):
-
-                i = epoch // 25
-
-                # fisher = FisherInformation(self.compiler)
-                # fisher.fisher_information_matrix, fisher.fisher_eigenvalues = fisher.batched_fisher_information(self.X_train, params)
-
-                # qfisher = QuantumFisherInformation(self.compiler)
-                # qfisher.quantum_fisher_information_matrix, qfisher.quantum_fisher_eigenvalues = qfisher.batched_quantum_fisher_information(self.X_train, params)
-
-
-                # y_pred = self.qnn(self.data, params)
-
-                data = LearnModelData(
-                    # quantum_fisher_information=qfisher,
-                    # fisher_information=fisher,
-                    params=params,
-                    # y_pred=y_pred
-                )
-
+            if epoch % 25 == 0:
+                data = LearnModelData(params=params)
                 data.epoch = epoch
-                
                 self.training_data.append(data)
 
         # store trained parameters
         self.fit_params = params
 
-
     def score_model(self):
+
+        '''
+            score the fitted model on test and training set
+        '''
+
+        assert self.fit_params != None, "need to train the model first"
 
         # Evaluate the cross entropy loss on the training set
         yp = jnp.array(self.qnn(self.X_train, self.fit_params)).T
@@ -176,6 +202,22 @@ class ClassifierQNN():
 
 
     def plot_fit(self, decoded_data:ndarray = None, show:bool = False):
+
+        '''
+            plot the fitted model as a two dimensional scatter plot, not-applicable to models where the input 
+            dimension is greater than 2. 
+
+            **kwargs:
+                decoded_data: the original data used prior to autoencoding, necessary to produce coherent plots
+                show: boolean to choose to show the plot
+
+            returns: figure with plotted axes
+        '''
+
+        if type(decoded_data) == ndarray: 
+            assert decoded_data.shape[1] == 2, "the input data must be 2-dimensional"
+        else: 
+            assert self.X_train.shape[1] == 2, "the input data must be 2-dimensional"
 
         figure = plt.figure()
         cm = plt.cm.RdBu
@@ -260,12 +302,33 @@ class ClassifierQNN():
     
 
     def fourier_coefficents(self, n_samples:int = 100, n_coeffs:int = 5, show:bool = False, ax = None):
+
+        '''
+            randomly sample fourier coefficents given the classification model
+
+            **kwargs:
+                n_samples: the number of samples to take
+                n_coeffs: the number of fourier coefficents to sample
+                show: boolean to verify whether to show the plot
+                ax: axes to plot the results onto where necessary
+
+            returns: figure with plotted axes
+        '''
+
         self.fourier.random_sample(n_coeffs, n_samples)
         return self.fourier.plot_coeffs(show, ax)
 
-    
 
     def plot_fisher_histogram(self, show:bool = False, ax = None):
+
+        '''
+            obtain the classical fisher matrices for each of the saved states during the training process
+            loops over the training data list, for each element assigns a FisherInformation class to each elements fisher_information property 
+
+            **kwargs:
+                show: boolean to verify whether to show the plot
+                ax: axes to plot the results onto where necessary
+        '''
         
         assert self.training_data != None, "need to train the model first"
 
@@ -298,10 +361,19 @@ class ClassifierQNN():
             ax[i].set_title(state.epoch)
             ax[i].boxplot(state.fisher_information.eigenvalues.reshape(-1))
 
-        plt.show()
+        if show: plt.show()
 
 
     def plot_quantum_fisher(self, show:bool = False, ax = None):
+
+        '''
+            obtain the quantum fisher matrices for each of the saved states during the training process
+            loops over the training data list, for each element assigns a QuantumFisherInformation class to each elements fisher_information property 
+
+            **kwargs:
+                show: boolean to verify whether to show the plot
+                ax: axes to plot the results onto where necessary
+        '''
 
         assert self.training_data != None, "need to train the model first"
 
